2230c3b937216d44a7005b89cb6f9dc7469bb478
Robin Haveneers <haveneersrobin@gmail.com>
2017-04-08 10:14:45 +0200

    Adding part 3

--- project 3/face_recognition.py
+++ project 3/face_recognition.py
@@ -1,6 +1,7 @@
 import cv2
 import cv2.cv as cv
 import os
+import errno
 import numpy as np
 import fnmatch
 
@@ -9,42 +10,52 @@ def create_database(directory, show = True):
     '''
     Process all images in the given directory.
     Every image is cropped to the detected face, resized to 100x100 and save in another directory (orignal directory name + "2").
-    
+
     @param directory:    directory to process
     @param show:         bool, show all intermediate results
-    
+
     '''
     #load a pre-trained classifier
     cascade = cv2.CascadeClassifier("data/haarcascade_frontalface_alt.xml")
+    
     #loop through all files
     for filename in fnmatch.filter(os.listdir(directory),'*.jpg'):
         file_in = directory+"/"+filename
+        dir_out = directory+"2/"
         file_out= directory+"2/"+filename
         img = cv2.imread(file_in)
         if show:
             cv2.imshow('img',img)
             cv2.waitKey(0)
-        #do face detection    
+
+        #do face detection
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         gray = cv2.equalizeHist(gray)
         rects = cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
         rects[:,2:] += rects[:,:2]
         rects = rects[0,:]
+
         #show detected result
         if show:
             vis = img.copy()
             cv2.rectangle(vis, (rects[0], rects[1]), (rects[2], rects[3]), (0, 255, 0), 2)
             cv2.imshow('img', vis)
             cv2.waitKey(0)
-        #crop image to the rectangle and resample it to 100x100 pixels 
-        
-        result = #TODO
-        
+
+        #crop image to the rectangle and resample it to 100x100 pixels
+        result = cv2.resize(img[rects[1]:rects[3],rects[0]:rects[2]],(100,100))
+
         #show result
         if show:
             cv2.imshow('img', result)
-            cv2.waitKey(0)            
-        #save the face in a second directory
+            cv2.waitKey(0)
+        # Save the face in a second directory
+        # First we check if the directory exists and if not, create it
+        try:
+            os.makedirs(dir_out)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
         cv2.imwrite(file_out, result)
     cv2.destroyAllWindows()
 
@@ -87,15 +98,15 @@ def pca(X, nb_components=0):
     @param X:                np.array containing the samples
                              shape = (nb samples, nb dimensions of each sample)
     @param nb_components:    the nb components we're interested in
-    @return: return the nb_components largest eigenvalues and eigenvectors of the covariance matrix and return the average sample 
+    @return: return the nb_components largest eigenvalues and eigenvectors of the covariance matrix and return the average sample
     '''
     [n,d] = X.shape
     if (nb_components <= 0) or (nb_components>n):
         nb_components = n
 
     #TODO
-    
-    return 
+
+    return
 
 def normalize(img):
     '''
@@ -107,13 +118,13 @@ if __name__ == '__main__':
     #create database of normalized images
     for directory in ["data/arnold", "data/barack"]:
         create_database(directory, show = False)
-    
+
     show = True
-    
+
     #create big X arrays for arnold and barack
     Xa = createX("data/arnold2")
     Xb = createX("data/barack2")
-            
+
     #Take one part of the images for the training set, the rest for testing
     nbTrain = 6
     Xtest = np.vstack( (Xa[nbTrain:,:],Xb[nbTrain:,:]) )
@@ -130,22 +141,22 @@ if __name__ == '__main__':
                                  normalize(eigenvectorsa[:,1].reshape(100,100)),
                                  normalize(eigenvectorsa[:,2].reshape(100,100)))
                                ).astype(np.uint8))
-    cv2.waitKey(0) 
+    cv2.waitKey(0)
     cv2.imshow('img',np.hstack( (mub.reshape(100,100),
                                  normalize(eigenvectorsb[:,0].reshape(100,100)),
                                  normalize(eigenvectorsb[:,1].reshape(100,100)),
                                  normalize(eigenvectorsb[:,2].reshape(100,100)))
                                ).astype(np.uint8))
-    cv2.waitKey(0) 
-            
+    cv2.waitKey(0)
+
     nbCorrect = 0
     for i in range(Xtest.shape[0]):
         X = Xtest[i,:]
-        
+
         #project image i on the subspace of arnold and barack
         Ya = project(eigenvectorsa, X, mua )
         Xa= reconstruct(eigenvectorsa, Ya, mua)
-        
+
         Yb = project(eigenvectorsb, X, mub )
         Xb= reconstruct(eigenvectorsb, Yb, mub)
         if show:
@@ -153,7 +164,7 @@ if __name__ == '__main__':
             cv2.imshow('img',np.hstack( (X.reshape(100,100),
                                          np.clip(Xa.reshape(100,100), 0, 255),
                                          np.clip(Xb.reshape(100,100), 0, 255)) ).astype(np.uint8) )
-            cv2.waitKey(0)   
+            cv2.waitKey(0)
 
         #classify i
         if np.linalg.norm(Xa-Xtest[i,:]) < np.linalg.norm(Xb-Xtest[i,:]):
@@ -163,7 +174,6 @@ if __name__ == '__main__':
         print str(i)+":"+str(bestC)
         if bestC == Ctest[i]:
             nbCorrect+=1
-    
+
     #Print final result
     print str(nbCorrect)+"/"+str(len(Ctest))
-    
